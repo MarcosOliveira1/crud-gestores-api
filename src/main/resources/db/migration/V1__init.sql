cba8f046c60766df2777c24dbc2094f83fc89554
marcos andre de oliveira braga <marcos.braga@poupex.com.br>
2020-11-23 00:02:10 -0300

    Migrations

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE public.gestor
+(
+  id serial NOT NULL,
+  data_nascimento date,
+  matricula character varying(255),
+  nome character varying(255),
+  setor character varying(255),
+  CONSTRAINT gestor_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.gestor
+  OWNER TO postgres;
